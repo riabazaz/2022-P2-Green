63418bdcb15a5bee0510a68ea35e9b3a53071eb4
hewyyy <hewbolan@umich.edu>
2022-04-06 15:48:13 -0400

    .

--- myarmsim.py
+++ myarmsim.py
@@ -18,7 +18,7 @@ class MyArmSim(ArmAnimatorApp):
       Tws2w = asarray([
            [1,0,0, 0], 
            [0,1,0, -5],
-           [0,0,1,-5],
+           [0,0,1,-10],
            [0,0,0,  1]
       ])
       ###
