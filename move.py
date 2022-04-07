39e12621c7872e373146a399c5f3eaa6f0a78e5b
hewyyy <hewbolan@umich.edu>
2022-04-07 00:01:09 -0400

    green team 2020 test

---
+++ move.py
@@ -0,0 +1,59 @@
+
+import tinyik
+from joy.plans import Plan
+from joy import progress
+from numpy import linspace,zeros,pi,rad2deg
+from scipy.interpolate import griddata
+
+#armSpec = asarray([
+#        [0,0.02,1,5,0],
+#        [0,1,0,5,1.57],
+#        [0,1,0,5,0],
+#      ]).T
+
+class Move( Plan ):
+    def __init__(self,app,*arg,**kw):
+        Plan.__init__(self,app,*arg,**kw)
+        self.app = app
+        self.steps = []
+        #Make another arm with the same orientation and arm lenght segments as
+        #what is defined in armSpec. This is what is used for inverse kinematics.
+        self.moveArm = tinyik.Actuator(['z',[5.,0.,0.],'y',[5.,0.,0.],'y',[5.,0.,0.]])
+        self.moveArm.angles = self.app.armSpec[-1,:]
+        self.pos = []   #Goal position for move. Will either be grid point or square corner. 
+        self.calibrated = False
+        self.CalDone = False
+        self.square = False
+        self.currentPos = []
+        self.curr = 0
+    #used to get initial joint angles before autonomous move
+    def syncArm(self):
+        ang = zeros(len(self.app.arm))
+        for i,motor in enumerate(self.app.arm):
+            ang[i] = motor.get_goal()*(pi/18000.)   #convert angles from centidegrees to radians
+        self.moveArm.angles = ang
+    
+    #for moving towwards desired position
+    def behavior(self):
+        angOffset = zeros(len(self.app.arm))
+        if self.calibrated == True:
+            #Interpolate between angle grid positions and angle error to calculate angle offset --> calibration
+            progress('Offset applied')
+            angOffset = griddata(self.app.calib_grid[:,:-2],self.app.calib_ang,(self.pos[:-2]),method='linear')[0]
+
+        self.syncArm()     
+        if self.CalDone == False or self.square == False:
+            #forward kinematics - get current end effector position given joint angles
+            self.currentPos = self.app.idealArm.getTool(self.moveArm.angles)        
+        #Create a number of evenly spaced steps between current position and goal position
+        self.steps = linspace(self.currentPos,self.pos,5)[:,:-1]    #Can adjust number of steps.
+        #execute movement along path of steps
+        for stepCount,step in enumerate(self.steps):
+            progress('Step #%d, %s' % (stepCount,str(step)))
+            self.app.currStep = stepCount
+            self.moveArm.ee = step
+            for i,motor in enumerate(self.app.arm):
+                #Calculate angles to move each step and move the motors 
+                motor.set_pos(rad2deg(self.moveArm.angles[i]+angOffset[i])*100)    #feed in angle to set_pos as centidegrees
+            yield self.forDuration(4)
+        progress('Move complete')
--- myarmsim.py
+++ myarmsim.py
@@ -5,111 +5,199 @@ Created on Wed Apr  1 14:23:10 2020
 
 @author: shrevzen
 """
-from curses import KEY_DOWN, KEY_LEFT
-from joy.decl import progress, KEYDOWN, K_UP, K_DOWN, K_LEFT, K_RIGHT
-from numpy import asarray
+import sys
+import os
+from numpy import linspace,dot,zeros,pi,asarray,meshgrid,ones,c_,save,load,array
 from p2sim import ArmAnimatorApp
-from math import pi
-import numpy as np
+from arm import Arm
+from joy.decl import KEYDOWN,K_k,K_o
+from joy import progress
+from move import Move
 
 class MyArmSim(ArmAnimatorApp):
-    def __init__(self,Tp2ws,**kw):
+    def __init__(self,Tp2ws,x,y,s,**kw):
       ###
       ### Student team selection -- transform from workspace coordinates to world
-      ### 
+      ###
+      
+      #first three columns represent axis. Last column represents
+      #translation. Adjust last column to adjust workspace placement relative to arm.
+      #Base of arm is always at world origin 
       Tws2w = asarray([
            [1,0,0, -5], 
            [0,1,0, -12],
            [0,0,1,0],
            [0,0,0,  1]
       ])
-
       ###
       ### Arm specification
       ###
+      
+      #Each row represents an arm segment.
+      #First three columns represent joint rotation axis. Fourth column represents
+      #arm segment length. Last column represents initial arm segment angle.
       armSpec = asarray([
-        [0,0.0001,1,0,-1.57], # base rotation around the z-axis
+        [0,0.02,1,0,-1.57], # base rotation around the z-axis
         [0,1,0,5,-1.57], # arm rotation around the y-axis #1.57
         [0,1,0,5,0] #the arm extending/unextending 
       ]).T
-
-      ## Drawing shit
-      self.current_goal = 0
-      self.calibMotorCoords = []
-
+      self.armSpec = armSpec
       ArmAnimatorApp.__init__(self,armSpec,Tws2w,Tp2ws,
-        simTimeStep=0.1, # Real time that corresponds to simulation time of 0.1 sec
+        simTimeStep=0.25, # Real time that corresponds to simulation time of 0.1 sec
         **kw
       )
-
+      self.idealArm = Arm(armSpec)  #create instance of arm class without real-life properties
+      self.move = Move(self)     #move plan
+     
     def show(self,fvp):
       fvp.plot3D([0],[0],[0],'^k',ms=10) # Plot black triangle at origin
+      
+      #plot square corners on paper in red
+      for point in self.square_w:
+          fvp.plot3D(*point[:-1],marker='o',color='r')
+      
+      #plot steps robot arm will take from current position to next corner/calibration point in green
+      for i,point in enumerate(self.move.steps):
+          if self.currStep and self.currStep == i:
+            fvp.plot3D(*point,marker='o',color='b')     #dot that robot is currently trying to move to is blue
+          else:
+            fvp.plot3D(*point,marker='o',color='g')
+      #plot calibration grid points in magenta
+      for point in self.calib_grid:
+          fvp.plot3D(*point[:-1],marker='o',color='m')
       return ArmAnimatorApp.show(self,fvp)
 
     def onStart(self):
       ArmAnimatorApp.onStart(self)
+
       ###
       ### TEAM CODE GOES HERE
       ###
 
-    ###
-    ### TEAM event handlers go here
-    ###    Handle events as you see fit, and return after
-    def on_K_r(self,evt):
-      progress("(say) r was pressed")
-  
+      #Define 4 corners in paper coordinates
+      #columns(left to right): x,y,z coordinates, 1's
+      #scale s represents full length of square side
+      square_p = asarray([
+                  [x-0.5*s, y+0.5*s, 0, 1],     #upper left
+                  [x+0.5*s, y+0.5*s, 0, 1],     #uper right
+                  [x+0.5*s, y-0.5*s, 0, 1],     #bottom right
+                  [x-0.5*s, y-0.5*s, 0, 1]      #bottom left
+                  ])
+
+      #Convert all coordinates for square to world coordinates
+      self.square_w = dot(square_p, self.Tp2w.T)
+      
+      ##Calibration
+      
+      #Create calibration grid on paper. These are points to move to during calibration.
+      nx,ny = (2,2)     #can be adjusted to add more calibration points
+      x_lin = linspace(0,8,nx)
+      y_lin = linspace(0,11,ny)
+      xv,yv = meshgrid(x_lin,y_lin,indexing='xy')
+      grid = c_[xv.reshape(nx*ny,1), yv.reshape(nx*ny,1), zeros((nx*ny,1)), ones((nx*ny,1))]
+      grid_idx = list(range(nx*ny))
+      for i in range(nx-(nx % 2)):
+          idx = nx*(2*i+1)
+          grid_idx[idx:idx+nx] = grid_idx[idx:idx+nx][::-1]
+      self.grid_idx = grid_idx
+      self.calib_grid = dot(grid,self.Tp2w.T)
+      self.calib_idx = 0
+      
+      #if calibration file exists, load calibration array in here, and skip over next part
+      #also set calibrated == true so that it calculates offset
+      #manually delete existing calibration array file before moving on to new arena
+      if(os.path.exists("calib_array.npy")):
+          self.calib_ang = load("calib_array.npy")
+          print(type(self.calib_ang))
+          self.move.calibrated = True
+          print(self.calib_ang[:,:-1])
+        
+     #printout of calibration matrix
+#      self.calib_ang = [[-1.54566359  0.00872665 -0.03543018]
+#                         [ 0.74228853  0.511556   -2.08479579]
+#                         [ 2.45148947 -0.61191244  2.08584299]
+#                         [-0.80826198  0.66479591 -2.20469991]]
+      
+      else:
+          #This is the matrix you save your angles in and use to calculate angle offset
+          self.calib_ang = zeros((nx*ny,len(self.arm)))    
 
     def onEvent(self,evt):
-      # Ignore everything except keydown events
-
+      ###
+      ### TEAM CODE GOES HERE
+      ###    Handle events as you see fit, and return after
+      
+      #activate autonomous mode and move to a square corner by pressing 'w','e','r','t'
       if evt.type == KEYDOWN:
-        if evt.key == K_UP:
-          # perform calibration
-          self.calibrate.start()
-          
-        if evt.key == K_DOWN:
-          # save calibration points
-          #changed from get_goal to get_pos
-          self.calibMotorCoords.append(np.array([self.arm[x].get_goal() for x in range(3)])) 
-          progress("goal angles: " + str(self.arm[0].get_goal()) 
-            + ", " + str(self.arm[1].get_goal()) 
-            + ", " + str(self.arm[2].get_goal()))
-          return
-
-        if evt.key == K_RIGHT:
-          self.saveResult(withCSV=False)
-          for i in range(3):
-            self.arm[i].set_pos(self.calibMotorCoords[self.current_goal][i])
-          self.current_goal += 1
-          return
-
-        p = "asd".find(evt.unicode)
-        if p>=0:
-          self.arm[p].set_pos(self.arm[p].get_goal() + 100)
-          return
-        # row of 'z' in QWERTY keyboard decrements motors
-        p = "zxc".find(evt.unicode)
-        if p>=0:
-          self.arm[p].set_pos(self.arm[p].get_goal() - 100)
-          return
+          progress('in keydown')
+          p = "wert".find(evt.unicode)
+          if p>=0:
+              if self.move.isRunning():
+                  progress('Move running!')
+                  return
+              self.move.pos = self.square_w[p]   #set square corner as goal position
+              self.move.start()
+              self.move.square = True
+              #after each move, set the previous goal position as your new starting position
+              self.move.currentPos = self.move.pos
+              progress('Move plan started!')
+              return
+          #Press 'k' to move to grid point for calibration
+          if evt.key == K_k:
+              self.move.pos = self.calib_grid[self.calib_idx]    #set next grid point as goal position
+              self.move.start()
+              progress('Moving to calibration point')
+              for i,motor in enumerate(self.arm):
+                  self.calib_ang[self.calib_idx,i] = motor.get_goal()*(pi/18000) #convert angles from centidegrees to radians
+        #Manual adjustment before this step  
+          #Press 'o' to calculate error between where arm moved and where it was supposed to be
+          if evt.key == K_o:
+              #Calculate error
+              progress('Calculate error')
+              if self.calib_idx < len(self.calib_ang):
+                  real_ang = zeros(len(self.arm))
+                  for i,motor in enumerate(self.arm):
+                      real_ang[i] = motor.get_goal()*(pi/18000)   
+                  self.calib_ang[self.calib_idx] = real_ang - self.calib_ang[self.calib_idx]
+                  self.calib_idx += 1
+                  self.move.calDone = True
+                  self.move.currentPos = self.move.pos
+              progress('Error calculation complete')
+              if self.calib_idx == len(self.calib_ang):
+                  progress('Calibration_complete!')
+                  self.move.calibrated = True
+                  save("calib_array.npy",self.calib_ang)    #save calibration array
+                  return
+          #manual movements
+          # row of 'a' on QWERTY keyboard increments motors
+          p = "asdf".find(evt.unicode)
+          if p>=0:
+            progress('manual move')
+            self.arm[p].set_pos(self.arm[p].get_goal() + 300)
+            return
+          # row of 'z' in QWERTY keyboard decrements motors
+          p = "zxcv".find(evt.unicode)
+          if p>=0:
+            progress('manual move')
+            self.arm[p].set_pos(self.arm[p].get_goal() - 300)
+            return
       return ArmAnimatorApp.onEvent(self,evt)
 
-if __name__=="__main__":
-  # Transform of paper coordinates to workspace
+
+if __name__=="__main__": 
+# Transform of paper coordinates to workspace
   Tp2ws = asarray([
        [0.7071,0,-0.7071,0],
        [0,     1,      0,0],
        [0.7071,0, 0.7071,0],
        [0,     0,      0,1]
   ])
-  Tp2ws=asarray([[  1.  ,   0.  ,   0.  ,   0.16],
-       [  0.  ,   0.71,   0.71,   1.92],
-       [  0.  ,  -0.71,   0.71,  10.63],
-       [  0.  ,   0.  ,   0.  ,   1.  ]])
-  app = MyArmSim(Tp2ws,
-     ## Uncomment the next line (cfg=...) to save video frames;
-     ## you can use the frameViewer.py program to view those
-     ## frames in real time (they will not display locally)
-     # cfg=dict(logVideo="f%04d.png")
-    )
+    #Initial test
+    # 
+  app = MyArmSim(Tp2ws,x,y,s
+                    ## Uncomment the next line (cfg=...) to save video frames;
+                    ## you can use the frameViewer.py program to view those
+                    ## frames in real time (they will not display locally)
+                    #      cfg=dict(logVideo="f%04d.png")
+                    )
   app.run()
