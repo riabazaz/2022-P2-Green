31bb3851a794083e311227241009a0e8fb72edd1
hewyyy <hewbolan@umich.edu>
2022-04-09 15:01:03 -0400

    .

--- move.py
+++ move.py
@@ -57,8 +57,8 @@ class Move( Plan ):
         self.moveArm.ee = self.pos[:3]
         largest_angle = 0
         for i,motor in enumerate(self.app.arm):
-            if abs(motor.get_pos(rad2deg(self.moveArm.angles[i])*100)) > largest_angle:
-                largest_angle = abs(motor.get_pos())
+            if abs(rad2deg(motor.get_pos())) > largest_angle:
+                largest_angle = abs(rad2deg(motor.get_pos()))
         self.steps = max(1,round(largest_angle * self.step_constant))
 
         #Create a number of evenly spaced steps between current position and goal position
