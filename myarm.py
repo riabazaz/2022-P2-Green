7aacc9ed6296135de900474e9d29fe2f5f1483bf
hewyyy <hewbolan@umich.edu>
2022-04-16 13:06:34 -0400

    .

--- myarm.py
+++ myarm.py
@@ -67,17 +67,18 @@ class MyArm(JoyApp):
     def createGrid(self, x_spacing, y_spacing):
       # create the array for the x points
       x_lin = [0]
-      while x_lin[-1]+x_spacing*0.394 < 8:
-        x_lin.append(x_lin[-1]+x_spacing*0.394)
+      while x_lin[-1]+x_spacing < 20: # 20cm is roughly 8inch
+        x_lin.append(x_lin[-1]+x_spacing) # *0.394 
       
       # create the array for the y points
       y_lin = [0]
-      while y_lin[-1]+y_spacing*0.394 < 11:
-        y_lin.append(y_lin[-1]+y_spacing*0.394)      
+      while y_lin[-1]+y_spacing < 27: # 27cm is roughly 11inch 
+        y_lin.append(y_lin[-1]+y_spacing) 
 
       nx, ny = (len(x_lin),len(y_lin))     #can be adjusted to add more calibration points
       xv,yv = meshgrid(x_lin,y_lin,indexing='xy')
       grid = c_[xv.reshape(nx*ny,1), yv.reshape(nx*ny,1), zeros((nx*ny,1)), ones((nx*ny,1))]
+      progress(str(grid))
       grid_idx = list(range(nx*ny))
       for i in range(nx-(nx % 2)):
           idx = nx*(2*i+1)
